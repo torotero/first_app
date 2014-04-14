90423528c53e120db3fe250a6549485a548b3f80
torotero <ganza.toro@gmail.com>
2014-04-14 11:22:40 +0700

    making first app lear from railstutorial.org

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '4d69a22854d540d4f3175750bf36b5bc148bc5c62f09709a3d69e0047b39f1ca229262203fc9d0203ec2e353ea5e683fd10326019ee427c3c1ee26f300735ba2'
